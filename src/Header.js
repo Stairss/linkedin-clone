36b3a33db9c6f11567e207fc562f29c0630ccdd7
Dawid Spisak <dawidspisak@gmail.com>
2021-05-10 21:33:18 +0200

    add HeaderOption

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
+import HeaderOption from './HeaderOption'
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
                 <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="logo" />
                 <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
                 </div>
             </div>
             <div className="header__right">
-                <SearchIcon />
-                <input type="text" />
+                <HeaderOption title="Home" Icon={HomeIcon} />
+                <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+                <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
+                <HeaderOption title="Messaging" Icon={ChatIcon} />
+                <HeaderOption title="Notification" Icon={NotificationsIcon} />
+                <HeaderOption
+                    avatar={'https://i.pinimg.com/originals/29/24/89/292489e7d0bf8ce7d5ffd81be62d0800.png'}
+                    title="Stairss"
+                />
+
             </div>
         </div>
     )
